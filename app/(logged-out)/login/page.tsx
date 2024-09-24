844d4e8b6a44031d6800db7b233d1b90043f0373
Pavel Klochkov <paul.klochkov@gmail.com>
2024-09-24 15:21:54 +0200

    Start login card

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -1,3 +1,36 @@
+'use client';
+
+import { PersonStandingIcon } from 'lucide-react';
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/Button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/Card';
+
 export default function LoginPage() {
-  return <div>Login</div>;
+  return (
+    <>
+      <PersonStandingIcon size={50} />
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login to your SupportMe account</CardDescription>
+        </CardHeader>
+
+        <CardContent>Login form</CardContent>
+        <CardFooter className="justify-between">
+          <small>Don't have an account?</small>
+          <Button asChild variant="outline" size="sm">
+            <Link href="/sign-up">Sign up</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
 }
