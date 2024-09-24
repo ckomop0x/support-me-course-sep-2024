7f9f9b1f8b947355bbbdb62649f5b49af18ae9e5
Pavel Klochkov <paul.klochkov@gmail.com>
2024-09-24 11:47:07 +0200

    Add shared layout

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button";
+
+export default function LandingPage() {
+    return (
+        <div>
+            <h1>Support Me</h1>
+            <Button>Login</Button>
+            <Button>Sing Up</Button>
+        </div>
+    )
+}
